9440b9d4e329442a6ebdaec9edc7131b1611b61d
Viktoriya Bortnikova <vikavika233@gmail.com>
2022-08-19 18:58:20 +0400

    initial commit

--- app/components/screens/wordsList/WordsList.tsx
+++ app/components/screens/wordsList/WordsList.tsx
@@ -14,9 +14,10 @@ export const WordsList: FC = () => {
 			title="Learn new words"
 			description="Learn new words from your dictionary"
 		>
-		<DekorHeading text="Luğət" className='text-center' />
+		<DekorHeading text="Lüğət" className='text-center' />
 
-			<Tooltip text="Hello, girls! This, is my super text. Now it is bigger than ever. Do you know English?" />
+			<Tooltip 
+			text="As has been the case for many years, jobs, or forms of employment wherein employees perform a service or duty in exchange for financial compensation, play a prominent role in society. Furthermore, all jobs—even those of seemingly little significance—are important, as they simply wouldn't exist if their specific responsibilities weren't of value to employers (companies or persons that pay others for their work), customers (individuals who pay money for a product or service), and the economy generally. Teachers, or educational professionals tasked with helping students understand certain subjects and topics, are especially crucial today. In short, teachers help their students to become qualified for their future careers. Doctors, or medical professionals who specialize in providing health-related assistance to patients, are some of the most respected individuals in America and the world. It's the responsibility of doctors to help those who feel less-than-stellar to determine the underlying health issue(s) and recommend an effective treatment (or remedy to a disease, disorder, or condition)." />
 		</Meta>
 	)
 }
--- app/components/ui/heading/Heading.tsx
+++ app/components/ui/heading/Heading.tsx
@@ -8,7 +8,7 @@ interface IHeading {
 export const Heading: FC<IHeading> = ({ title, className }) => {
 	return (
 		<h1
-			className={`text-white text-opacity-80 font-semibold first-letter: ${
+			className={`text-black text-opacity-80 font-semibold first-letter: ${
 				className?.includes('xl') ? '' : 'text-3xl'
 			} ${className} `}
 		>
