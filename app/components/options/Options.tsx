6e0da9a9ee301a74bdaf1734ac8fb881894be882
Viktoriya Bortnikova <vikavika233@gmail.com>
2023-06-22 15:53:08 +0400

    changed links on home page

--- app/components/options/Options.tsx
+++ app/components/options/Options.tsx
@@ -5,9 +5,9 @@ export const Options = () => {
 	const router = useRouter()
 	return (
 		<div className="flex flex-wrap items-center justify-evenly max-w-[100%]">
-			<Option title="A1" path="/courses/course-1" />
-			<Option title="A2" path="/courses/course-2" />
-			<Option title="B1" path="/courses/course-3" />
+			<Option title="A1" path="/courses?level=A1" />
+			<Option title="A2" path="/courses?level=A2" />
+			<Option title="B1" path="/courses?level=B1" />
 
 			{/*	<Image src={fon} width={261} height={254} alt="text" draggable={false} />
 			<Image src={fon} width={261} height={254} alt="text" draggable={false} />
